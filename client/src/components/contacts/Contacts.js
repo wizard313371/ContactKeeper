4514ab2df51af82113b20a6699ccde945fddb249
wizard <vagin31337@gmail.com>
2019-08-18 10:13:40 +0500

    auth

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,11 +1,12 @@
-import React, { useContext, Fragment } from 'react';
+import React, {useContext, Fragment} from 'react';
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from "./ContactItem";
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
-    const { contacts, filtered } = contactContext;
+    const {contacts, filtered} = contactContext;
 
     if (contacts.length === 0) {
         return <h4>Please add a contact</h4>
@@ -13,11 +14,17 @@ const Contacts = () => {
 
     return (
         <Fragment>
-            {filtered !== null ? filtered.map(contact =>
-                (<ContactItem key={contact.id} contact={contact} />
-            )) : (contacts.map(contact =>
-                <ContactItem key={contact.id} contact={contact} />
-            ))}
+            <TransitionGroup>
+                {filtered !== null ? filtered.map(contact =>
+                    (<CSSTransition key={contact.id} timeout={500} className='item'>
+                            <ContactItem contact={contact}/>
+                        </CSSTransition>
+                    )) : (contacts.map(contact =>
+                    <CSSTransition key={contact.id} timeout={500} className='item'>
+                        <ContactItem contact={contact}/>
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </Fragment>
     );
 };
