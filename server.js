2ebd4391b2f48da79cbe415a70bbd8105a9ea380
wizard <assa113@mail.ru>
2019-08-19 22:49:17 +0500

    production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 //Connect Database
 connectDB();
@@ -16,5 +17,11 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
